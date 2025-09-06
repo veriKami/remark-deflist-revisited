477dd0f052aee6b87ef2d4b5a96e644ac469e0a3
Kami Weronika Amelia <verikami@gmail.com>
2025-09-06 09:45:00 +0200

    +: esbuild.config.js

---
+++ esbuild.config.js
@@ -0,0 +1,11 @@
+// esbuild.config.js
+import { build } from "esbuild";
+
+build({
+  entryPoints: ["src/index.js"],
+  bundle: true, //: BUNDLE dependencies
+  outfile: "dist/index.js",
+  // platform: 'neutral',
+  platform: "node",
+  format: "esm"
+});
