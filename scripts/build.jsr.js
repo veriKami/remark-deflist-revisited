4ea1784292fbc6902a14290871264176bd843b4c
Kami Weronika Amelia <verikami@gmail.com>
2025-09-07 08:53:58 +0200

    test: reverse

--- scripts/build.jsr.js
+++ scripts/build.jsr.js
@@ -52,7 +52,7 @@ for (const file of libFiles) {
 
 //: 4. Transform imports for JSR
 //: --------------------------------------------------------
-/*/
+//*/
 const pkgJsonPath = path.resolve("package.json");
 const pkgJson = JSON.parse(fs.readFileSync(pkgJsonPath, "utf8"));
 const deps = { ...pkgJson.dependencies, ...pkgJson.devDependencies, ...pkgJson.peerDependencies };
