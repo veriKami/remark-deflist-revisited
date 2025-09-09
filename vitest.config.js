64d0595b0b58a9451a28aa7c4152c7510f170562
Kami Weronika Amelia <verikami@gmail.com>
2025-09-09 15:06:39 +0200

    tests: config

--- vitest.config.js
+++ vitest.config.js
@@ -4,6 +4,10 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     //: Default: ['**/*.{test,spec}.?(c|m)[jt]s?(x)']
-    include: ["tests/**/*.{test,spec}.?(c|m)[jt]s?(x)"]
+    include: [
+      "tests/**/*.src.{test,spec}.?(c|m)[jt]s?(x)",
+      "tests/**/*.dist.{test,spec}.?(c|m)[jt]s?(x)",
+    ],
+    reporters: "verbose",
   },
 });
