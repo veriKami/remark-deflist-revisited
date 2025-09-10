4d4a4b2a34cc79bd63ce0b122c211d3cfd77e50c
Kami Weronika Amelia <verikami@gmail.com>
2025-09-10 16:24:33 +0200

    config

--- eslint.config.js
+++ eslint.config.js
@@ -6,12 +6,23 @@ import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
+    ignores: [
+        "node_modules/",
+        "coverage/",
+        "dist/",
+        "lib/",
+        "demo/",
+        "deno/",
+        "**/__*",
+    ],
+  },
+  {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
     plugins: { js },
     extends: ["js/recommended"],
     languageOptions: {
       globals: globals.node
-    }
+    },
   },
   tseslint.configs.recommended,
 ]);
